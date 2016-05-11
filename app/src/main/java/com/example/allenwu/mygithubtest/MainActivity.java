6c7cc7c7276aa1a8b12aed762049b12ad984010c
allenwusyong <k125286605@gmail.com>
2016-05-11 23:23:00 +0800

    add new function clickevent

--- app/src/main/java/com/example/allenwu/mygithubtest/MainActivity.java
+++ app/src/main/java/com/example/allenwu/mygithubtest/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.allenwu.mygithubtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+public static final String TAG="Mainactivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -12,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
 
 
         //this is small chaange
+
+    }
+    public void showNumberLogcat(View view){
+        doMathstuff();
+    }
+
+    private  void doMathstuff(){
+
+        int a=10;
+        int b=20;
+        int total=a+b;
+        Log.d(TAG,"sum---"+total);
     }
 }
