d3c7f0c136f1eb230f400e71d98d73f200f3e6b5
allenwusyong <k125286605@gmail.com>
2016-05-11 23:00:27 +0800

    new class  --AndroidItem

---
+++ app/src/main/java/com/example/allenwu/mygithubtest/AndroidItem.java
@@ -0,0 +1,25 @@
+package com.example.allenwu.mygithubtest;
+
+/**
+ * Created by Admin on 2016/5/11.
+ */
+public class AndroidItem {
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImgurl() {
+        return imgurl;
+    }
+
+    public void setImgurl(String imgurl) {
+        this.imgurl = imgurl;
+    }
+
+    private   String title;
+ private    String imgurl;
+}
